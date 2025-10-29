859ae9a5ec34ddda3dfb5fba94036d4786c2f0aa
kkademorais <kaike.carvalho@outlook.com>
2025-10-28 23:48:17 -0300

    initial commit: putting Spring initial archives into repo

---
+++ src/main/java/com/sideproject/gestao_vendas_nfe/GestaoVendasNfeApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.gestao_vendas_nfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVendasNfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVendasNfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/gestao_vendas_nfe/GestaoVendasNfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.gestao_vendas_nfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVendasNfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
