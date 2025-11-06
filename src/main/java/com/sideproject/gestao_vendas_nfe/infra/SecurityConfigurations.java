b6b061038e7ca64f57c1a1c0956199a373399ac3
kkademorais <kaike.carvalho@outlook.com>
2025-11-05 23:58:12 -0300

    feat: added SecurityConfigurations first configs, initiated SecurityFilterChain configs, need to create authentication endpoints

---
+++ src/main/java/com/sideproject/gestao_vendas_nfe/infra/SecurityConfigurations.java
@@ -0,0 +1,32 @@
+package com.sideproject.gestao_vendas_nfe.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+        // Manipulam o pipeline do filtro de segurança pra determinar o formato Stateless da autenticação
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/products")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                        )
+                .build();
+    }
+
+
+
+}
